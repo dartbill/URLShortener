4f1cc2d88fcb5aa530e70c88c55961d39f6aafd9
dartbill <billieldartnell@hotmail.com>
2022-07-06 10:52:32 +0100

    add home route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import os 
@@ -7,6 +7,11 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+
+@app.route('/')
+def hello():
+    return jsonify({"hello": "world"}),200
 
 
 class CarsModel(db.Model):
