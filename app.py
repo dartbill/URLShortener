0faa67d8a6b5ebcc63f9ac15321081f1e682aaa9
dartbill <billieldartnell@hotmail.com>
2022-07-06 10:39:28 +0100

    test

--- app.py
+++ app.py
@@ -4,12 +4,45 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
-class User(db.Model):
+class CarsModel(db.Model):
+    __tablename__ = 'cars'
+
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    name = db.Column(db.String())
+    model = db.Column(db.String())
+    doors = db.Column(db.Integer())
+
+    def __init__(self, name, model, doors):
+        self.name = name
+        self.model = model
+        self.doors = doors
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return f"<Car {self.name}>"
+
+
+@app.route('/cars', methods=['POST', 'GET'])
+def handle_cars():
+    if request.method == 'POST':
+        if request.is_json:
+            data = request.get_json()
+            new_car = CarsModel(name=data['name'], model=data['model'], doors=data['doors'])
+            db.session.add(new_car)
+            db.session.commit()
+            return {"message": f"car {new_car.name} has been created successfully."}
+        else:
+            return {"error": "The request payload is not in JSON format"}
+
+    elif request.method == 'GET':
+        cars = CarsModel.query.all()
+        results = [
+            {
+                "name": car.name,
+                "model": car.model,
+                "doors": car.doors
+            } for car in cars]
+
+        return {"count": len(results), "cars": results}
