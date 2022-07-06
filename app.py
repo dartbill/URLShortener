f74485ba9ca43ef427dbce6d0079522599064b01
dartbill <billieldartnell@hotmail.com>
2022-07-06 11:09:33 +0100

    add if statement

--- app.py
+++ app.py
@@ -36,14 +36,14 @@ class CarsModel(db.Model):
 @app.route('/cars', methods=['POST', 'GET'])
 def handle_cars():
     if request.method == 'POST':
-        # if request.is_json:
+        if request.is_json:
             data = request.get_json()
             new_car = CarsModel(name=data['name'], model=data['model'], doors=data['doors'])
             db.session.add(new_car)
             db.session.commit()
             return {"message": f"car {new_car.name} has been created successfully."}
-        # else:
-            # return {"error": "The request payload is not in JSON format"}
+        else:
+            return {"error": "The request payload is not in JSON format"}
 
     elif request.method == 'GET':
         cars = CarsModel.query.all()
