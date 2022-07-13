e14750f5b0a3b8ca8e7c82807ce33643f199b96e
dartbill <billieldartnell@hotmail.com>
2022-07-13 10:59:02 +0100

    add link to home page

--- app.py
+++ app.py
@@ -60,7 +60,8 @@ def members_handler():
         db.session.add(new_url)
         # below saves the data
         db.session.commit()
-        return redirect(f'https://flaskshorturl.herokuapp.com/{short_url}')
+        # return redirect(f'https://flaskshorturl.herokuapp.com/{short_url}')
+        return render_template('index.html', link=f'https://flaskshorturl.herokuapp.com/{short_url}')
         # return {'WORKING '}
 
 # this is the home route should just render the for
